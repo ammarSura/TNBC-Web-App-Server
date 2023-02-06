1b3248085c4350cdf86bf0b30c024c7553140e10
ammarSura <ammarsura@gmail.com>
2023-02-06 12:21:00 +0530

    feat: refresh token schema

--- src/schemas/RefreshToken.ts
+++ src/schemas/RefreshToken.ts
@@ -1,11 +1,10 @@
-import { Schema, model, connect } from 'mongoose';
-import { IRefreshToken } from '../types/types';
+import { Schema, model, connect } from 'mongoose'
+import { type IRefreshToken } from '../types/types'
 
 export const RefreshToken = new Schema<IRefreshToken>({
-    token: { type: Schema.Types.String, required: true },
-    userId: { type: Schema.Types.String, required: true },
-    expiresAt: { type: Schema.Types.Number, required: true }
+  token: { type: Schema.Types.String, required: true },
+  userId: { type: Schema.Types.String, required: true },
+  expiresAt: { type: Schema.Types.Number, required: true }
 })
 
 export default model('RefreshToken', RefreshToken)
-
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,39 +1,40 @@
-import { Response } from 'express'
+import { type Response } from 'express'
 
-export type IUser = {
-    name: string
-    email: string
-    designation?: string
+export interface IUser {
+  name: string
+  email: string
+  designation?: string
+  isAdmin?: boolean
 }
 
-export type ICoors = {
-    id: string
-    x: number
-    y: number
+export interface ICoors {
+  id: string
+  x: number
+  y: number
 }
 
-export type IAlgorithmMap = {
-    algorithm: 'MESMR' | 'otherAlgo'
-    annotCoors: ICoors
-    correctCounter: number
+export interface IAlgorithmMap {
+  algorithm: 'MESMR' | 'otherAlgo'
+  annotCoors: ICoors
+  correctCounter: number
 }
 
-export type IImage = {
-    id: string
-    url: string
-    algorithmMap: IAlgorithmMap[],
-    annotsAssessed: number,
-    size?: string
+export interface IImage {
+  id: string
+  url: string
+  algorithmMap: IAlgorithmMap[]
+  annotsAssessed: number
+  size?: string
 }
 
-export type JWTData = {
-    userId: string
+export interface JWTData {
+  userId: string
 }
 
 export type IRoute = (user: JWTData, params: any, res: Response) => Promise<any>
 
-export type IRefreshToken = {
-    token: string
-    userId: string
-    expiresAt: number
+export interface IRefreshToken {
+  token: string
+  userId: string
+  expiresAt: number
 }
