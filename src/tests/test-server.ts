aa2337f1b7a22ccfac5329f6b8705644a0375e31
ammarSura <ammarsura@gmail.com>
2023-02-10 00:12:09 +0530

    refac: unused vars

--- src/tests/test-server.ts
+++ src/tests/test-server.ts
@@ -1,8 +1,4 @@
 import makeServer from '../utils/initialise-server'
-const PORT = 3001
-const INIT_MESSAGE = 'Test app initialised'
-const DB_NAME = 'tnbc-test'
-const CONNECTION_URI = `mongodb://localhost:27017/${DB_NAME}`
 
 const app = makeServer()
 
