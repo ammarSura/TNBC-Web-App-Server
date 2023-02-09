3e8b4d04640cc45cf98d0f983cb69f9827c63941
ammarSura <ammarsura@gmail.com>
2023-02-09 18:36:37 +0530

    feat: moved error handling and returning date from server to util

--- src/utils/handler-express.ts
+++ src/utils/handler-express.ts
@@ -9,8 +9,16 @@ const handler = async (route: IRoute, c: Context, res: Response) => {
   const body = c.request.body
   const user = c.security.bearerAuth
   const req = { ...query, ...params, ...body }
-  await route(user, req, res)
-
+  try {
+    const response = await route(user, req, res)
+    res.status(200).json(response)
+  } catch(error) {
+    if(error.isBoom) {
+      return res.status(error.output.statusCode).json({
+        message: error.message
+      })
+    }
+  }
   logger.info({
     method: req.method,
     path: req.path,
