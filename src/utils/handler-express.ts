f4c35c4faa3a8556295d5a75cad42c52b252a9b3
ammarSura <ammarsura@gmail.com>
2023-02-09 18:54:01 +0530

    feat: logging and error handling improved

--- src/utils/handler-express.ts
+++ src/utils/handler-express.ts
@@ -1,32 +1,40 @@
 import { type Context } from 'openapi-backend'
-import { type IRoute } from '../types/types'
+import { JWTData, type IRoute } from '../types/types'
 import { type Response } from 'express'
 import logger from './logger'
 
 const handler = async (route: IRoute, c: Context, res: Response) => {
   const query = c.request.query
   const params = c.request.params
+  //path params
   const body = c.request.body
-  const user = c.security.bearerAuth
+  const user: JWTData = c.security.bearerAuth
   const req = { ...query, ...params, ...body }
+  const contextRequest = c.request
+  const result: {[key: string]: any} = {
+    method: contextRequest.method,
+    path: contextRequest.path,
+    query: Object.keys(query).length > 0 ? query : undefined,
+    body: Object.keys(body).length > 0 ? body : undefined,
+  }
   try {
     const response = await route(user, req, res)
     res.status(200).json(response)
+    result.body = response
+    result.statusCode = 200
   } catch(error) {
     if(error.isBoom) {
-      return res.status(error.output.statusCode).json({
+      res.status(error.output.statusCode).json({
         message: error.message
       })
+      if(error.output.statusCode === 500) {
+        logger.error(error)
+      }
+      result.errorMessage = error.message
+      result.statusCode = error.output.statusCode
     }
   }
-  logger.info({
-    method: req.method,
-    path: req.path,
-    query: req.query,
-    body: req.body,
-    // result: res.son,
-    status: res.statusCode
-  })
+  logger.info(result)
 }
 
 export default handler
